8e49ce71fa26000bea920882dff1cb0921f3ca14
Bankole Esan <techybanky@gmail.com>
2022-05-22 02:35:22 +0100

    first deploy

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   transpileDependencies: ["vuetify"],
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
-      args[0].title = "Nodens Inc";
+      args[0].title = "Lugah Audio";
       return args;
     });
   },
